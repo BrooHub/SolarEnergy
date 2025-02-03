5523b75a9383a4647d4377fb53023079a35170d1
Ahmedsall1 <as4619272@gmail.com>
2025-02-03 13:06:39 +0300

    update search page routes

--- tkw/lib/screens/search_page.dart
+++ tkw/lib/screens/search_page.dart
@@ -139,6 +139,27 @@ class _SearchPageState extends State<SearchPage> {
                       sunshineHours: selectedIlce!['sunshineHours'],
                       ilce: selectedIl!['il']);
                   activ = true;
+                  if (AppState.selectedDistrict == null) {
+                    AppState.selectedDistrict = District(
+                        postaCode: selectedIlce!['posta_code'],
+                        ilce: selectedIl!['il'],
+                        latitude: selectedIlce!['latitude'],
+                        longitude: selectedIlce!['longitude'],
+                        monthlyKWh: selectedIlce!['monthlyKWh'],
+                        sunshineHours: selectedIlce!['monthlyKWh']);
+                  } else {
+                    AppState.selectedDistrict?.ilce = selectedIl!['il'];
+                    AppState.selectedDistrict?.latitude =
+                        selectedIlce!['latitude'];
+                    AppState.selectedDistrict?.longitude =
+                        selectedIlce!['longitude'];
+                    AppState.selectedDistrict?.monthlyKWh =
+                        selectedIlce!['monthlyKWh'];
+                    AppState.selectedDistrict?.sunshineHours =
+                        selectedIlce!['sunshineHours'];
+                    AppState.selectedDistrict?.postaCode =
+                        selectedIlce!['posta_code'];
+                  }
                 });
               },
             ),
@@ -153,12 +174,18 @@ class _SearchPageState extends State<SearchPage> {
               children: [
                 TextCard(
                     text: 'Daha Fazla Sonuç \nGörmek İçin Tıklayın',
-                    onPressed: () {}),
-                TextCard(
-                    text: 'Enerji Üretimini \nHesaplamak İçin \nTıklayın',
                     onPressed: () {
-                      Navigator.pushNamed(context, '/result_summary');
+                      Navigator.pushNamed(context, '/web_view');
                     }),
+                activ == true
+                    ? TextCard(
+                        text: 'Enerji Üretimini \nHesaplamak İçin \nTıklayın',
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/result_summary');
+                        })
+                    : TextCard(
+                        text: 'Enerji Üretimini \nHesaplamak İçin \nTıklayın',
+                        onPressed: () {})
               ],
             ),
           ],
